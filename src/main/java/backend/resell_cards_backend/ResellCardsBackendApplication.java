fccf4929f2e6c17c6a6f541cb14d5bd0bad14167
yoman321 <luophilipe@gmail.com>
2024-05-28 16:55:34 -0400

    first commit

---
+++ src/main/java/backend/resell_cards_backend/ResellCardsBackendApplication.java
@@ -0,0 +1,13 @@
+package backend.resell_cards_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellCardsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResellCardsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/resell_cards_backend/ResellCardsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.resell_cards_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellCardsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
