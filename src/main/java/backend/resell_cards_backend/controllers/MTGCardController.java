01f84f38bd469143e9c04695ba29595a873c53e0
yoman321 <luophilipe@gmail.com>
2024-06-16 19:43:15 -0400

    removed unused imports

--- src/main/java/backend/resell_cards_backend/controllers/MTGCardController.java
+++ src/main/java/backend/resell_cards_backend/controllers/MTGCardController.java
@@ -1,39 +1,37 @@
 package backend.resell_cards_backend.controllers;
 
-import backend.resell_cards_backend.domains.Card;
 import backend.resell_cards_backend.domains.MTGCard;
 import backend.resell_cards_backend.services.MTGCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.*;
 
 @RestController
 @RequestMapping(path = "api/mtg_inventory")
 @CrossOrigin(origins = "http://localhost:5173")
-public class MTGCardController{
+public class MTGCardController {
 
-    private final MTGCardService mtgCardService;
+  private final MTGCardService mtgCardService;
 
-    @Autowired
-    public MTGCardController(MTGCardService mtgCardService){
-        this.mtgCardService = mtgCardService;
-    }
+  @Autowired
+  public MTGCardController(MTGCardService mtgCardService) {
+    this.mtgCardService = mtgCardService;
+  }
 
-    @GetMapping
-    public List<MTGCard> getMTGCards(){
-        return mtgCardService.removeIdFromCards(mtgCardService.getMTGInventory());
-    }
+  @GetMapping
+  public List<MTGCard> getMTGCards() {
+    return mtgCardService.removeIdFromCards(mtgCardService.getMTGInventory());
+  }
 
-    @PostMapping
-    public void addNewMTGCard(@RequestBody MTGCard mtgCard){
-        mtgCardService.addNewMTGCard(mtgCard);
-    }
+  @PostMapping
+  public void addNewMTGCard(@RequestBody MTGCard mtgCard) {
+    mtgCardService.addNewMTGCard(mtgCard);
+  }
 
-    @DeleteMapping
-    public void removeMTGCard(@RequestBody MTGCard mtgCard){
-        mtgCardService.removeMTGCard(mtgCard);
-    }
+  @DeleteMapping
+  public void removeMTGCard(@RequestBody MTGCard mtgCard) {
+    mtgCardService.removeMTGCard(mtgCard);
+  }
 
 }
