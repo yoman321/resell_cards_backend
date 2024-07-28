459d27c19bae9fc582ca76e6ce0eaa41c81e25be
yoman321 <luophilipe@gmail.com>
2024-07-28 12:35:44 -0400

    updated mtg card types to be an array

--- src/main/java/backend/resell_cards_backend/configs/MTGCardConfig.java
+++ src/main/java/backend/resell_cards_backend/configs/MTGCardConfig.java
@@ -1,6 +1,5 @@
 package backend.resell_cards_backend.configs;
 
-import backend.resell_cards_backend.enums.TCG;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.CommandLineRunner;
@@ -14,12 +13,13 @@ import backend.resell_cards_backend.enums.MTGCardType;
 @Configuration
 public class MTGCardConfig {
 
-    @Bean
-    CommandLineRunner secondCommandLineRunner(MTGCardRepository mtgCardRepository) {
-        return args -> {
-            MTGCard card1 = new MTGCard(1L, "A Card name", MTGCardType.ARTIFACT, "Eldraine", 12L);
-            MTGCard card2 = new MTGCard(2L, "Another card name", MTGCardType.PLANESWALKER, "Theros", 10L);
-            mtgCardRepository.saveAll(List.of(card1, card2));
-        };
-    }
+  @Bean
+  CommandLineRunner secondCommandLineRunner(MTGCardRepository mtgCardRepository) {
+    return args -> {
+      MTGCard card1 = new MTGCard(1L, "A Card name",
+          new MTGCardType[] { MTGCardType.ARTIFACT, MTGCardType.PLANESWALKER }, "Eldraine", 12L);
+      MTGCard card2 = new MTGCard(2L, "Another card name", new MTGCardType[] { MTGCardType.BATTLE }, "Theros", 10L);
+      mtgCardRepository.saveAll(List.of(card1, card2));
+    };
+  }
 }
--- src/main/java/backend/resell_cards_backend/domains/MTGCard.java
+++ src/main/java/backend/resell_cards_backend/domains/MTGCard.java
@@ -6,86 +6,84 @@ import jakarta.persistence.*;
 import backend.resell_cards_backend.enums.MTGCardType;
 import backend.resell_cards_backend.enums.TCG;
 
-
 @Entity
-public class MTGCard extends Card{
+public class MTGCard extends Card {
 
-    @Id
-    @SequenceGenerator(
-        name = "mtg_card_sequence",
-        sequenceName = "mtg_card_sequence",
-        allocationSize = 1
-    )
+  @Id
+  @SequenceGenerator(name = "mtg_card_sequence", sequenceName = "mtg_card_sequence", allocationSize = 1)
 
-    @GeneratedValue(
-        strategy = GenerationType.SEQUENCE,
-        generator = "mtg_card_sequence"
-    )
-    private Long id;
-    private String mtgCardName;
-    private MTGCardType mtgCardType;
-    private String mtgCardEdition;
-    private Long mtgCardValue;
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mtg_card_sequence")
+  private Long id;
+  private String mtgCardName;
+  private MTGCardType mtgCardType[];
+  private String mtgCardEdition;
+  private Long mtgCardValue;
 
-    public MTGCard(){
-        super(TCG.MTG);
-    }
+  public MTGCard() {
+    super(TCG.MTG);
+  }
 
-    public MTGCard(Long id, String mtgCardName, MTGCardType mtgCardType, String mtgCardEdition, Long mtgCardValue){
-        super(TCG.MTG);
-        this.id = id;
-        this.mtgCardName = mtgCardName;
-        this.mtgCardType = mtgCardType;
-        this.mtgCardEdition = mtgCardEdition;
-        this.mtgCardValue = mtgCardValue;
-    }
+  public MTGCard(Long id, String mtgCardName, MTGCardType mtgCardType[], String mtgCardEdition, Long mtgCardValue) {
+    super(TCG.MTG);
+    this.id = id;
+    this.mtgCardName = mtgCardName;
+    this.mtgCardType = mtgCardType;
+    this.mtgCardEdition = mtgCardEdition;
+    this.mtgCardValue = mtgCardValue;
+  }
 
-    public MTGCard(String mtgCardName, MTGCardType mtgCardType, String mtgCardEdition, Long mtgCardValue){
-        super(TCG.MTG);
-        this.mtgCardName = mtgCardName;
-        this.mtgCardType = mtgCardType;
-        this.mtgCardEdition = mtgCardEdition;
-        this.mtgCardValue = mtgCardValue;
-    }
+  public MTGCard(String mtgCardName, MTGCardType mtgCardType[], String mtgCardEdition, Long mtgCardValue) {
+    super(TCG.MTG);
+    this.mtgCardName = mtgCardName;
+    this.mtgCardType = mtgCardType;
+    this.mtgCardEdition = mtgCardEdition;
+    this.mtgCardValue = mtgCardValue;
+  }
 
-    public Long getId(){
-        return id;
-    }
-    public String getMtgCardName(){
-        return mtgCardName;
-    }
-    public MTGCardType getMtgCardType(){
-        return mtgCardType;
-    }
-    public String getMtgCardEdition(){
-        return mtgCardEdition;
-    }
-    public Long getMtgCardValue(){
-        return mtgCardValue;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    //we only have the value setter because once the card is created, we don't want any other values to be changed
-    public void setMTGCardValue(Long mtgCardValue) {
-        this.mtgCardValue = mtgCardValue;
-    }
+  public String getMtgCardName() {
+    return mtgCardName;
+  }
 
-    @Override
-    public MTGCard removeIdFromCard(){
-       return new MTGCard(getMtgCardName(), getMtgCardType(), getMtgCardEdition(), getMtgCardValue());
-    }
+  public MTGCardType[] getMtgCardType() {
+    return mtgCardType;
+  }
 
-    @Override
-    public String toString() {
-        return "{" +
-            " id='" + getId() + "'" +
-            ", mtgCardName='" + getMtgCardName() + "'" +
-            ", mtgCardType='" + getMtgCardType() + "'" +
-            ", mtgCardEdition='" + getMtgCardEdition() + "'" +
-            ", value='" + getMtgCardValue() + "'" +
-            "}";
-    }
+  public String getMtgCardEdition() {
+    return mtgCardEdition;
+  }
+
+  public Long getMtgCardValue() {
+    return mtgCardValue;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  // we only have the value setter because once the card is created, we don't want
+  // any other values to be changed
+  public void setMTGCardValue(Long mtgCardValue) {
+    this.mtgCardValue = mtgCardValue;
+  }
+
+  @Override
+  public MTGCard removeIdFromCard() {
+    return new MTGCard(getMtgCardName(), getMtgCardType(), getMtgCardEdition(), getMtgCardValue());
+  }
+
+  @Override
+  public String toString() {
+    return "{" +
+        " id='" + getId() + "'" +
+        ", mtgCardName='" + getMtgCardName() + "'" +
+        ", mtgCardType='" + getMtgCardType() + "'" +
+        ", mtgCardEdition='" + getMtgCardEdition() + "'" +
+        ", value='" + getMtgCardValue() + "'" +
+        "}";
+  }
 
 }
